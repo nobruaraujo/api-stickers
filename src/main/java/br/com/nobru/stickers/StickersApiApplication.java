0c64c0b30b3f2b3e6e999f405ea8194a658248ee
Bruno Araujo <brunosousa357@gmail.com>
2024-10-01 13:21:51 -0300

    First Commit

---
+++ src/main/java/br/com/nobru/stickers/StickersApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.nobru.stickers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StickersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StickersApiApplication.class, args);
+	}
+
+}
